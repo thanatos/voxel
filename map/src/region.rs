3dfab61af9e9c16646a8207b5486ff29204ede8a
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2021-11-20 19:46:02 -0500

    Improvements to region files

--- map/src/region.rs
+++ map/src/region.rs
@@ -1,5 +1,5 @@
 use std::io::{self, Write};
-use std::path::PathBuf;
+use std::path::Path;
 
 use rusqlite::Connection;
 
@@ -30,47 +30,42 @@ impl ChunkCompression {
     }
 }
 
+/// A region file, which represents a large portion of the world; it contains "chunks".
+///
+/// A region file, under the hood, is an SQLite database.
 pub struct Region {
     connection: Connection,
 }
 
-fn run_schema_create(connection: &Connection) -> rusqlite::Result<()> {
-    const RAW_SQL: &str = include_str!("region_file_schema.sql");
-
-    connection.execute_batch(RAW_SQL)?;
-
-    let mime = serde_cbor::to_vec(&"application/vnd.voxel.region.v0").unwrap();
-    connection.execute(
-        "INSERT INTO metadata VALUES ('mimetype', ?);",
-        &[&mime],
-    )?;
-
-    Ok(())
-}
-
 impl Region {
-    pub fn create(path: PathBuf) -> Result<Region, RegionError> {
+    /// Create a new region file at the given path.
+    pub fn create(path: &Path) -> Result<Region, RegionError> {
         let connection = Connection::open_with_flags(
             path,
             rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
-        )?;
+        )
+        .map_err(RegionErrorKind::Sqlite)?;
 
-        run_schema_create(&connection)?;
+        run_schema_create(&connection).map_err(RegionErrorKind::Sqlite)?;
 
         Ok(Region { connection })
     }
 
-    pub fn open(path: PathBuf) -> Result<Option<Region>, RegionError> {
+    /// Open an existing region file at the given path.
+    pub fn open(path: &Path) -> Result<Option<Region>, RegionError> {
         // Ugh, there's a TOCTOU bug here, but the sqlite API doesn't let us avoid it.
         if !path.exists() {
             return Ok(None);
         }
 
         let connection =
-            Connection::open_with_flags(path, rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE)?;
+            Connection::open_with_flags(path, rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE)
+                .map_err(RegionErrorKind::Sqlite)?;
         Ok(Some(Region { connection }))
     }
 
+    /// Load a chunk from the region file. `chunk_coord` should contain a region-relative chunk
+    /// coordinate.
     pub fn load_chunk(&mut self, chunk_coord: &ChunkCoord) -> Result<Vec<u8>, RegionError> {
         let (compression, compressed_chunk_data) = self.connection.query_row(
             "\
@@ -89,15 +84,17 @@ WHERE
                     row.get_unwrap::<_, Vec<u8>>(1),
                 ))
             },
-        )?;
+        ).map_err(RegionErrorKind::Sqlite)?;
 
         let chunk_data = match compression {
             ChunkCompression::Brotli => {
                 let mut buf = Vec::new();
                 {
                     let mut decoder = brotli2::write::BrotliDecoder::new(&mut buf);
-                    decoder.write_all(&compressed_chunk_data)?;
-                    decoder.finish()?;
+                    decoder
+                        .write_all(&compressed_chunk_data)
+                        .map_err(RegionErrorKind::Io)?;
+                    decoder.finish().map_err(RegionErrorKind::Io)?;
                 }
                 buf
             }
@@ -105,6 +102,7 @@ WHERE
         Ok(chunk_data)
     }
 
+    /// Save a chunk to the region file.
     pub fn save_chunk(
         &mut self,
         chunk_coord: &ChunkCoord,
@@ -114,12 +112,15 @@ WHERE
         let compressed_data = {
             let mut buf = Vec::new();
             let mut encoder = brotli2::write::BrotliEncoder::new(&mut buf, 11);
-            encoder.write_all(&chunk_data)?;
-            encoder.finish()?;
+            encoder
+                .write_all(&chunk_data)
+                .map_err(RegionErrorKind::Io)?;
+            encoder.finish().map_err(RegionErrorKind::Io)?;
             buf
         };
 
-        self.connection.execute("\
+        self.connection.execute(
+            "\
 INSERT INTO chunks
 (chunk_x, chunk_y, chunk_z, compression, chunk_data)
 VALUES (?, ?, ?, ?, ?)
@@ -136,18 +137,41 @@ DO UPDATE SET
                 compression.as_int(),
                 compressed_data,
             ],
-        )?;
+        ).map_err(RegionErrorKind::Sqlite)?;
 
         Ok(())
     }
 }
 
+/// An error while doing operations on a region file.
 #[derive(Debug, thiserror::Error)]
-pub enum RegionError {
+#[error(transparent)]
+pub struct RegionError(RegionErrorKind);
+
+impl From<RegionErrorKind> for RegionError {
+    fn from(err: RegionErrorKind) -> RegionError {
+        RegionError(err)
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+enum RegionErrorKind {
     #[error("sqlite error: {0}")]
     Sqlite(#[from] rusqlite::Error),
     #[error("sqlite error: expected 1 row, got {0} while {1}")]
     ExpectedOneRow(i64, &'static str),
     #[error("I/O error: {0}")]
-    IoError(#[from] io::Error),
+    Io(#[from] io::Error),
+}
+
+/// Create the SQL tables, etc. (schema) in a new SQLite region file.
+fn run_schema_create(connection: &Connection) -> rusqlite::Result<()> {
+    const RAW_SQL: &str = include_str!("region_file_schema.sql");
+
+    connection.execute_batch(RAW_SQL)?;
+
+    let mime = serde_cbor::to_vec(&"application/vnd.voxel.region.v0").unwrap();
+    connection.execute("INSERT INTO metadata VALUES ('mimetype', ?);", &[&mime])?;
+
+    Ok(())
 }
